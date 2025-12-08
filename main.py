1de4e5c9d70df1169a9526e3258b8b30269cee97
Gustavo-2l <dri.khedira@gmail.com>
2025-12-08 14:23:18 -0300

    Remove duplicate origin from CORS configuration

--- main.py
+++ main.py
@@ -25,8 +25,7 @@ app = FastAPI(title="Finanpro - API de Finanças")
 # CONFIGURAÇÃO DO CORS PARA O FRONTEND
 # ======================================
 origins = [
-    "https://finanpro-front-main-atualizado-lpaa-igapw8fnu.vercel.app",
-    "https://finanpro-front-main-atualizado-lpaa.vercel.app/"
+    "https://finanpro-front-main-atualizado-lpaa.vercel.app"
 ]
 
 
